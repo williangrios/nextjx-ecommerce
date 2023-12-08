b43fe8aab327e857501a8677c9490886ff13ad7b
willian <williangrios@yahoo.com.br>
2023-12-08 07:47:05 -0300

    feat: created interfaces

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,14 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors:{
+        primary: '#000',
+      },
     },
   },
   plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
 }
 export default config
