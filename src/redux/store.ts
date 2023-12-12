c74c0aa9ae52e3670e3df426cd97a95abdd6ca5f
willian <williangrios@yahoo.com.br>
2023-12-12 17:26:56 -0300

    feat: add redux features

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit"
 import { userSlice } from './userSlice'
+import { cartSlice } from "./cartSlice"
+
+let initialCartItems = []
+if (typeof window !== 'undefined') {
+    initialCartItems = JSON.parse(localStorage.getItem('cartItems') || '[]')
+}
 
 const store = configureStore({
     reducer: {
-        user: userSlice.reducer
+        user: userSlice.reducer,
+        cart: cartSlice.reducer
+    },
+    preloadedState: {
+        cart: {
+            cartItems: initialCartItems,
+            cartTotal: 0
+        }
     }
 })
 
